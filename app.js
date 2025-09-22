9d508d77831447e0f02a81876f73b6fa4b804cfb
Andy <andygt10@outlook.es>
2025-09-22 12:47:21 -0500

    aregada la funcion de sortear amigos

--- app.js
+++ app.js
@@ -9,5 +9,27 @@ function agregarAmigo() {
         document.getElementById("amigo").value = "";
         mostrarAmigos();
     }
-    console.log(amigos);
+}
+
+function mostrarAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    amigos.forEach(function(amigo) {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("Debes ingresar al menos dos amigos para realizar el sorteo.");
+        return;
+    }
+    let amigo1 = amigos[Math.floor(Math.random() * amigos.length)];
+    let amigo2 = amigos[Math.floor(Math.random() * amigos.length)];
+    while (amigo1 === amigo2) {
+        amigo2 = amigos[Math.floor(Math.random() * amigos.length)];
+    }
+    document.getElementById("resultado").textContent = "El amigo secreto de " + amigo1 + " es " + amigo2;
 }
